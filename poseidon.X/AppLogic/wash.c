8d050279e8a4db5c30f9cf5ec12f4ef241458b69
awataki <awataki1206@gmail.com>
2020-01-24 05:50:37 +0900

    implements wash.c

--- poseidon.X/AppLogic/wash.c
+++ poseidon.X/AppLogic/wash.c
@@ -1,13 +1,38 @@
 #include "wash.h"
+#include "../Domain/PracticeBox.h"
 
-void update_wash_hardware_status(hard_ware_states hw){
+int ctoi(char c);
 
+void update_wash_hardware_status(hard_ware_states *hw){
+    (*hw).WATER_SENSOR = ctoi(read_Box_Sensor(1));
+    (*hw).LAUNDRY_SENSOR = ctoi(read_Box_Sensor(2));
 };
 
-void start(int c){
-
+void start_wash(hard_ware_states *hw,flags *f){
+    (*f).is_washing = 1;
+    // move tub
+    move_Box_Motor(1,1);
+    (*hw).TUB = 1;
+    // move wind
+    move_Box_Motor(2,1);
+    (*hw).WIND = 1;
+    return;
 };
 
-void stop(){
-
+void stop_wash(hard_ware_states *hw,flags *f){
+    (*f).is_washing = 0;
+    move_Box_Motor(1,0);
+    (*hw).TUB = 1;
+    move_Box_Motor(2,0);
+    (*hw).WIND = 1;
 };
+
+
+
+int ctoi(char c){
+    if(c == '1'){
+        return 1;
+    }else{
+        return 0;
+    }
+}
